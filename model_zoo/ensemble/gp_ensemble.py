c8a8118a162371a92735958446d80947c9d79709
samuelstanton <ss13641@nyu.edu>
2021-07-29 19:34:54 -0600

    disable GP ensemble

--- model_zoo/ensemble/gp_ensemble.py
+++ model_zoo/ensemble/gp_ensemble.py
@@ -1,26 +1,26 @@
-import torch
-
-from model_zoo.ensemble import BaseEnsemble
-from model_zoo.regression import DeepFeatureSVGP
-
-
-class GPEnsemble(BaseEnsemble):
-    """ Ensemble of SVGP + DKL regression models
-    """
-    def __init__(self, input_dim, target_dim, num_components,
-                 num_elites, submodule_params):
-        """
-        Args:
-            input_dim (int)
-            target_dim (int)
-            num_components (int)
-            num_elites (int)
-            submodule_params (dict): kwargs to pass to GP constructor)
-        """
-        super().__init__(input_dim, target_dim, num_components, num_elites)
-        components = [DeepFeatureSVGP(
-            input_dim=input_dim,
-            label_dim=target_dim,
-            **submodule_params
-        ) for _ in range(num_components)]
-        self.components = torch.nn.ModuleList(components)
+# import torch
+#
+# from model_zoo.ensemble import BaseEnsemble
+# from model_zoo.regression.dkl_svgp import DeepFeatureSVGP
+#
+#
+# class GPEnsemble(BaseEnsemble):
+#     """ Ensemble of SVGP + DKL regression models
+#     """
+#     def __init__(self, input_dim, target_dim, num_components,
+#                  num_elites, submodule_params):
+#         """
+#         Args:
+#             input_dim (int)
+#             target_dim (int)
+#             num_components (int)
+#             num_elites (int)
+#             submodule_params (dict): kwargs to pass to GP constructor)
+#         """
+#         super().__init__(input_dim, target_dim, num_components, num_elites)
+#         components = [DeepFeatureSVGP(
+#             input_dim=input_dim,
+#             label_dim=target_dim,
+#             **submodule_params
+#         ) for _ in range(num_components)]
+#         self.components = torch.nn.ModuleList(components)
